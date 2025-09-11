8c40e749086f633c73fd6011ffdd4094a1e93cb7
Daniel Ayala Guzmán <151292390+danguu@users.noreply.github.com>
2025-09-11 12:09:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import sys
 from lexer_core import lexer
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python3 main.py archivo.py")
-        sys.exit(1)
+archivo = input("Dame el archivo para analizar: ")
 
-    archivo = sys.argv[1]
+try:
+    with open(archivo, "r", encoding="utf-8") as f:
+        source = f.read()
+except:  # noqa: E722
+    print("No se pudo leer el archivo:", archivo)
+    exit
 
-    try:
-        with open(archivo, "r", encoding="utf-8") as f:
-            source = f.read()
-    except:
-        print("No se pudo leer el archivo:", archivo)
-        sys.exit(1)
+tokens = lexer(source)
 
-    tokens = lexer(source)
-
-    if tokens is not None:
-        # Guardar en archivo
-        with open("tokens.txt", "w", encoding="utf-8") as out:
-            for t in tokens:
-                out.write(t + "\n")
+if tokens is not None:
+    # Guardar en archivo
+    with open("tokens.txt", "w", encoding="utf-8") as out:
+        for t in tokens:
+            out.write(t + "\n")
